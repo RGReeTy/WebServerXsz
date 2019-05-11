e532056295e2d35cc0e3f6961893b4d8f15dfdba
RGReeTy <be.a.reety@gmail.com>
2019-05-11 23:41:27 +0300

    Added exception part of study

---
+++ src/Exceptions/ExceptionExample.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 0;
+		int c = a / b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/Exceptions/TryCatch1Example.java
@@ -0,0 +1,26 @@
+package Exceptions;
+
+public class TryCatch1Example {
+
+	public static void main(String[] args) {
+		try {
+			int a =2;
+			int b =0;
+			int c = a/b;
+			int[] arr = null;
+			System.out.println(arr[1]);
+			System.out.println(c);
+
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+
+		}
+
+	}
+
+}
