d3e1abd9b0b0ff720ce5206e34d1f62fd084099b
RGReeTy <be.a.reety@gmail.com>
2019-06-06 22:43:20 +0300

    Add Tread exercises

---
+++ src/Thread/RunnableTest.java
@@ -0,0 +1,33 @@
+package Thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class RunnableTest implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("RunnableTest finished");
+
+	}
+
+	public static void main(String[] args) {
+		Thread th = new Thread(new RunnableTest(), "RunnableTest");
+		th.start(); // th.run();
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Main finished");
+
+	}
+
+}
